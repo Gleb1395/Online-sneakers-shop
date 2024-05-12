7c935255352d4f46c322997e4247d185ca7be6f4
Глеб Дубинин <gleb1395@gmail.com>
2024-05-12 13:51:18 +0300

    Home_work

--- src/config/settings/dev.py
+++ src/config/settings/dev.py
@@ -6,12 +6,12 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-INSTALLED_APPS += []
+INSTALLED_APPS += []  # NOQA
 
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",  # NOQA
     }
 }
 
