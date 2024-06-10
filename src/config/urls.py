fcd4c91b3b9b340ddab68b81d8aa70944a2d6908
Глеб Дубинин <gleb1395@gmail.com>
2024-06-10 07:19:05 +0300

    Nginx

--- src/config/urls.py
+++ src/config/urls.py
@@ -21,7 +21,9 @@ from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 from rest_framework import permissions
 
-from sneakers_shop.views import AboutView, IndexView, ShopDetailView
+from sneakers_shop.views import (AboutView, CartListView, ContactUsView,
+                                 IndexView, ServicesView, ShopDetailView,
+                                 ShopListView)
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -41,6 +43,10 @@ urlpatterns = [
     path("", IndexView.as_view(), name="index"),
     path("about/", AboutView.as_view(), name="about"),
     path("detail/", ShopDetailView.as_view(), name="detail"),
+    path("service/", ServicesView.as_view(), name="service"),
+    path("contact-us/", ContactUsView.as_view(), name="contact-us"),
+    path("shop/", ShopListView.as_view(), name="shop"),
+    path("cart/", CartListView.as_view(), name="cart"),
     path("api-auth/", include("rest_framework.urls")),
     path("api/", include("api.urls")),
     path("docs/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
--- src/sneakers_shop/views.py
+++ src/sneakers_shop/views.py
@@ -12,3 +12,19 @@ class AboutView(TemplateView):
 
 class ShopDetailView(TemplateView):
     template_name = "detail.html"
+
+
+class ServicesView(TemplateView):
+    template_name = "service.html"
+
+
+class ContactUsView(TemplateView):
+    template_name = "contact_us.html"
+
+
+class ShopListView(TemplateView):
+    template_name = "shop.html"
+
+
+class CartListView(TemplateView):
+    template_name = "cart.html"
