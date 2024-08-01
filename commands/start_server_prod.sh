632ac52725d0a8c38d1d9fa4bc6bccb03ca4123b
Глеб Дубинин <gleb1395@gmail.com>
2024-08-01 17:12:34 +0300

    fix nginx

--- commands/start_server_prod.sh
+++ commands/start_server_prod.sh
@@ -3,5 +3,5 @@ python src/manage.py makemigrations
 python src/manage.py migrate
 python src/manage.py check
 python src/manage.py collectstatic --noinput
-gunicorn -w ${WSGI_WORKERS} -b 0:${WSGI_PORT} --chdir /src config.wsgi:applicarion --log-level=${WSGI_LOG_LEVEL}
+gunicorn -w ${WSGI_WORKERS} -b 0:${WSGI_PORT} --chdir /shop/src config.wsgi:application --log-level=${WSGI_LOG_LEVEL}
 python src/manage.py runserver 0:8010
